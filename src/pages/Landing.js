4be103ed20a580fa80bd16f8870fb329450fe328
MiraeKim <hufs202000512@gmail.com>
2023-09-20 21:49:41 +0900

    초기 라우팅 구성

---
+++ src/pages/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+function Landing() {
+    return (
+      <div className="App">
+        랜딩페이지
+      </div>
+    );
+  }
+  
+  export default Landing;
---
+++ src/pages/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+function Main() {
+    return (
+      <div className="App">
+        메인페이지
+      </div>
+    );
+  }
+  
+  export default Main;
