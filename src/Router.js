f4b9666ba9e8030f4a7ffbc715234a197193f06f
MiraeKim <hufs202000512@gmail.com>
2023-09-21 08:41:10 +0900

    feat: 랜딩페이지 구현

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Main from "./pages/Main";
+import Login from "./pages/Auth/Login";
 
 function DefaultRouter () {
     return (
@@ -9,6 +10,7 @@ function DefaultRouter () {
         <Routes>
             <Route path="/" element={<Landing />} />
             <Route path="/main" element={<Main />} />
+            <Route path="/login" element={<Login/>}/>
         </Routes>
     </Router>
     );
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,10 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 
+const OrangeBox = styled.div`
+  background-color:orange;
+  width: 33%;
+  height: 100vh;
+  float: left;
+`
+const Btn = styled.button`
+
+`
+
 function Landing() {
     return (
-      <div className="App">
-        랜딩페이지
+      <div>
+        <OrangeBox/>
+        <Btn>
+          챗봇 사용 ➜
+        </Btn>
       </div>
     );
   }
