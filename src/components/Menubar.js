20749b24bb5163f49d349b22bb890a63c074f1d0
MiraeKim <hufs202000512@gmail.com>
2023-09-22 13:44:23 +0900

    feat: 메뉴바 세팅

---
+++ src/components/Menubar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const Background = styled.div`
+    background-color: #eea849;
+`
+
+
+const Menubar =()=> {
+    return (
+      <Background>
+        메뉴바
+      </Background>
+    );
+  }
+  
+  export default Menubar;
